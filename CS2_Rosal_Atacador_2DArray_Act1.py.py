0e6fceb24fe35ccd1e0c60072a3525cf65be43c7
Dasein Danielle Rei N. Atacador <daseindanielle.atacador@smc.pshs.edu.ph>
2026-03-05 09:22:07 +0800

    Add files via upload

---
+++ CS2_Rosal_Atacador_2DArray_Act1.py.py
@@ -0,0 +1,9 @@
+temperature = [[24, 24, 22, 24, 24, 25, 15],
+               [24, 24, 24, 24, 26, 24, 25],
+]
+
+Baguio_weather = temperature[0][1:8]
+Davao_temperature = temperature[1][1:8]
+
+print("Baguio's temperature for the week:", Baguio_weather)
+print("Davao's temperature for the week:", Davao_temperature)
